b8087cbf14d9e080665deb4bf650d0a14a37199a
Bilal <bilal.mte@gmail.com>
2020-09-25 14:40:35 +0700

    merge from home-ui

--- src/services/uri.ts
+++ src/services/uri.ts
@@ -8,6 +8,6 @@ export function parseEcocURI(data: string) {
   return new EcocURI(data)
 }
 
-export function generateEcocURI(address:string, amount: string) {
+export function generateEcocURI(address: string, amount: string) {
   return new EcocURI({ address, amount }).toString()
 }
