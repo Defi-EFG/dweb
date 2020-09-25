3c0a7cae0271def1b9d4d203f79dd68c0426b710
tarkul <tarkulbeer@gmail.com>
2020-09-25 15:53:32 +0700

    edit page docs colum

--- src/components/Home/DoughnutChart.vue
+++ src/components/Home/DoughnutChart.vue
@@ -1,44 +1,40 @@
 <script>
-  import { Doughnut } from 'vue-chartjs'
+import { Doughnut } from 'vue-chartjs'
 
-  export default {
-    extends: Doughnut,
-    data () {
-      return {
-        chartData: {
-          labels:["Babol",	"Cabanatuan"],
-          datasets: [{
-              borderWidth: 1,
-              borderColor: [
-              '#3FC0DB',
-              '#4E4E4E',              
-              ],
-              backgroundColor: [
-              '#3FC0DB',
-              '#4E4E4E',           
-              ],
-              data: [1000,	500]
-            }]
+export default {
+  extends: Doughnut,
+  data() {
+    return {
+      chartData: {
+        labels: ['Babol', 'Cabanatuan'],
+        datasets: [
+          {
+            borderWidth: 1,
+            borderColor: ['#3FC0DB', '#4E4E4E'],
+            backgroundColor: ['#3FC0DB', '#4E4E4E'],
+            data: [1000, 500]
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
         },
-        options: {
-            legend: {
-                display: false
-            }, 
-            layout: {
-                    padding: {
-                        left: 50,
-                        right: 50,
-                        top: 0,
-                        bottom: 50
-                    }
-                },
-            responsive: true,
-            maintainAspectRatio: false
-        }
+        layout: {
+          padding: {
+            left: 50,
+            right: 50,
+            top: 0,
+            bottom: 50
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: false
       }
-    },
-    mounted () {
-      this.renderChart(this.chartData, this.options)
     }
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options)
   }
+}
 </script>
