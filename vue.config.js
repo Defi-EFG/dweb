8721d2516e50610dca4b6cb4443249925489639f
Napong T <58070033@kmitl.ac.th>
2020-12-04 15:56:20 +0700

    Add preload script to vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const PreloadWebpackPlugin = require('preload-webpack-plugin')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
+  chainWebpack: config => {
+    config.plugins.delete('prefetch')
+    config.plugin('preload').use(new PreloadWebpackPlugin())
+  },
   configureWebpack: {
     optimization: {
       runtimeChunk: 'single'
