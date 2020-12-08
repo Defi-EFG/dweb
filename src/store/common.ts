957e99730e4b12912211ce3a0b6e6f6f8197b9c3
Bilal <bilal.mte@gmail.com>
2020-12-08 12:10:31 +0700

    change api for production, get known tokenInfo from json file

--- src/store/common.ts
+++ src/store/common.ts
@@ -1,6 +1,6 @@
 import * as constants from '@/constants'
 import { Ecrc20, Currency } from '@/types/currency'
-import { ECOC, ETH, USDT, EFG, GPT, EWETH, EWUSDT } from '@/store/wallet/currency'
+import { ECOC, EFG, GPT, EWETH, EWUSDT } from '@/store/wallet/currency'
 
 export const loanCurrency = {
   name: EFG.name,
@@ -22,8 +22,6 @@ export const rewardCurrency = {
 
 export const getCurrency = (currencyName: string) => {
   if (currencyName === ECOC.name) return ECOC
-  if (currencyName === ETH.name) return ETH
-  if (currencyName === USDT.name) return USDT
   if (currencyName === EFG.name) return EFG
   if (currencyName === GPT.name) return GPT
   if (currencyName === EWETH.name) return EWETH
@@ -34,8 +32,6 @@ export const getCurrency = (currencyName: string) => {
 
 export const getCurrencyDecimals = (currencyName: string) => {
   if (currencyName === ECOC.name) return '8'
-  if (currencyName === ETH.name) return ETH.tokenInfo?.decimals as string
-  if (currencyName === USDT.name) return USDT.tokenInfo?.decimals as string
   if (currencyName === EFG.name) return EFG.tokenInfo?.decimals as string
   if (currencyName === GPT.name) return GPT.tokenInfo?.decimals as string
   if (currencyName === EWETH.name) return EWETH.tokenInfo?.decimals as string
@@ -45,8 +41,6 @@ export const getCurrencyDecimals = (currencyName: string) => {
 }
 
 export const getTokenInfo = (currencyName: string) => {
-  if (currencyName === ETH.name) return ETH.tokenInfo as Ecrc20
-  if (currencyName === USDT.name) return USDT.tokenInfo as Ecrc20
   if (currencyName === EFG.name) return EFG.tokenInfo as Ecrc20
   if (currencyName === GPT.name) return GPT.tokenInfo as Ecrc20
   if (currencyName === EWETH.name) return EWETH.tokenInfo as Ecrc20
